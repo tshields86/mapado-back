87c64c2b8db2821563ff27a9a07980acb2d232ec
Travis Shields <travis.shields@gmail.com>
2016-09-05 23:14:16 -0400

    Set up api and have get all and post working with the frontend

--- server/api/index.js
+++ server/api/index.js
@@ -4,40 +4,48 @@ import { ObjectID } from 'mongodb';
 
 export default function(){
   const api = Router();
-  const collection = db.get().collection('taco');
+  const collection = db.get().collection('tasks');
 
   // since we are in the api folder it the REAL route is localhost:3000/api
-  api.get('/tacos', (req, res) => {
-    //we finally hit the database!
+  api.get('/tasks', (req, res) => {
+    // get all tasks
+    console.log('Get all tasks');
     collection.find().toArray((err, docs) => {
-      res.json({ tacos : docs });
+      res.json({ tasks : docs });
     });
   });
 
-  api.get('/taco', (req, res) => {
+  api.get('/task', (req, res) => {
+    // get a single task
+    console.log('Get a task');
     collection.findOne({"_id": ObjectID(req.body._id)},(err, collection) => {
       res.json(collection)
     });
   });
 
-  api.post('/taco', (req, res) => {
-    // create a new taco
+  api.post('/task', (req, res) => {
+    // create a new task
+    console.log('Post a task');
+    console.log('req.body',req.body);
     collection.insert(req.body, (err, result) => {
       res.json(result);
     });
   });
 
-  api.put('/taco', (req, res) => {
-    // edit a taco
+  api.put('/task', (req, res) => {
+    // edit a task
+    console.log('Edit a task');
     collection.update({"_id": ObjectID(req.body._id)}, {"name": req.body.name, "toppings": req.body.toppings}, {w:1} , (err, result) => {
       res.json(result);
     });
 
   });
 
-  api.delete('/taco', (req, res) => {
-    // delete a whole damn taco
-    collection.remove({"_id": ObjectID(req.body._id)},(err, result) => {
+  api.delete('/task/:id', (req, res) => {
+    // delete a task
+    console.log('Delete a task');
+    // console.log("req.params.id:", req.params.id);
+    collection.remove({"_id": ObjectID(req.params.id)},(err, result) => {
       res.json(result);
     });
   });
